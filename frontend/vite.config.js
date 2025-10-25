e5cb7f216d473b8656742aa92bd2f3290089ca3a
BadalamentiAlejandro <badalamentialejandro@gmail.com>
2025-10-25 03:21:36 +0200

    Added conf to develop in remote.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-],
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+
+    server: {
+        // 💡 Esta sección para la detección de cambios en Docker.
+        watch: {
+        usePolling: true,
+        // Opcional: Ajustar el intervalo de polling (milisegundos).
+        interval: 100 
+        }
+    }
 })
